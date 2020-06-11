9cd61756bcc71a4f6d6659832cd56e6994fe35e0
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-12 00:31:17 +0200

    feat: add swagger route for api docs

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import v1Router from "./v1/routes";
 import CustomError from "./utils/customError";
 import errorHandler from "./utils/errorhandler";
+import { swaggerSpec } from "./utils/swaggerSpec";
+
+const swaggerUi = require("swagger-ui-express");
 
 // create express app
 const app = express();
@@ -18,6 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).send("Fury!");
 });
+
+// use swagger-Ui-express for your app documentation endpoint
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // router for api version 1
 app.use("/api/v1", v1Router);
