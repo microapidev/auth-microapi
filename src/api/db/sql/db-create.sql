c4599999b8d21eb6c251d7536c93595ea0d6bf2f
Dave <callremz@gmail.com>
2020-06-13 04:54:44 +0100

    fix: remove db objects ownership

--- src/api/db/sql/db-create.sql
+++ src/api/db/sql/db-create.sql
@@ -5,12 +5,6 @@ CREATE TABLE IF NOT EXISTS public.db_version (
 );
 
 
-ALTER TABLE public.db_version OWNER TO postgres;
-
---
--- Name: db_version_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
 CREATE SEQUENCE IF NOT EXISTS public.db_version_id_seq
     START WITH 1
     INCREMENT BY 1
@@ -18,19 +12,6 @@ CREATE SEQUENCE IF NOT EXISTS public.db_version_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE public.db_version_id_seq OWNER TO postgres;
-
---
--- Name: db_version_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.db_version_id_seq OWNED BY public.db_version.id;
-
-
---
--- Name: organizations; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE IF NOT EXISTS public.organizations (
     organization_name character varying NOT NULL,
@@ -40,33 +21,12 @@ CREATE TABLE IF NOT EXISTS public.organizations (
     password character varying NOT NULL
 );
 
-
-ALTER TABLE public.organizations OWNER TO postgres;
-
---
--- Name: organization_organization_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
 CREATE SEQUENCE IF NOT EXISTS public.organization_organization_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
-ALTER TABLE public.organization_organization_id_seq OWNER TO postgres;
-
---
--- Name: organization_organization_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.organization_organization_id_seq OWNED BY public.organizations.organization_id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE IF NOT EXISTS public.users (
     user_id bigint NOT NULL,
@@ -77,12 +37,6 @@ CREATE TABLE IF NOT EXISTS public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
 CREATE SEQUENCE IF NOT EXISTS public.users_user_id_seq
     START WITH 1
     INCREMENT BY 1
@@ -90,46 +44,16 @@ CREATE SEQUENCE IF NOT EXISTS public.users_user_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE public.users_user_id_seq OWNER TO postgres;
-
---
--- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.db_version ALTER COLUMN id SET DEFAULT nextval('public.db_version_id_seq'::regclass);
-
-
---
--- Name: organization_id; Type: DEFAULT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.organizations ALTER COLUMN organization_id SET DEFAULT nextval('public.organization_organization_id_seq'::regclass);
 
-
---
--- Name: user_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
-
-
---
--- Name: db_version_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.db_version
     DROP CONSTRAINT IF EXISTS db_version_pkey;
 ALTER TABLE ONLY public.db_version
     ADD CONSTRAINT db_version_pkey PRIMARY KEY (id);
-
 
 --
 -- Name: organization_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
@@ -175,18 +99,4 @@ ALTER TABLE ONLY public.users
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_organization_id_fkey FOREIGN KEY (organization_id) REFERENCES public.organizations(organization_id);
 
-
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
 
