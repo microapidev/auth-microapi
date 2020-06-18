c6e8bfa1800dacf6e54a0d7721e778ae0ff34b50
Nwankwo Henry <nameisifunanya@gmail.com>
2020-06-18 17:19:51 +0100

    create login route logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const db = require('../models')
+const db = require('../models');
+const User = require('../models/user');
 
 //**HARDCODED USER**//
 //POST - /api/auth/current-user
@@ -18,7 +19,20 @@ router.post('/register')
 
 //POST - /api/auth/current-user
 //msg - Login route
-router.post('/login')
+router.post('/login', async(req, res, next){		
+		
+	//Checking if email or username exists
+	const user = await User.findOne({$or:[{email:req.body.email},{username:req.body.username}]});
+
+	if(!user) return res.status(400).json({'message':'Invalid Email or Password'});
+
+	//Checking if password is correct
+	const validPassword = await bcrypt.compare(password, user.password);
+	if (!validPassword) return res.status(400).json({ 'message': 'Invalid Email or Password' });g
+
+
+
+});
 
 //POST - /api/auth/current-user
 //msg - LogOut route
