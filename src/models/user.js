27683cc908a0ef85719ba30aded33e8e39e5a4e1
Barnes <ohenesetwumasi@gmail.com>
2020-06-19 21:55:26 -0700

    Implement logout route

--- src/models/user.js
+++ src/models/user.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please enter an email"]
     },
+     username: {
+         type: String,
+         required: true,
+         unique: true
+     },
     password: {
         type: String,
         required: [true, "Please enter a Password"],
