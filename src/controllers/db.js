55db7ad187109c3a64d04415d84b917eaa438d49
Alkaseem <alkaseemabubakar27@gmail.com>
2020-06-25 16:34:53 +0100

    fix alot of bugs

---
+++ src/controllers/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose.set('debug', true);
+mongoose.Promise = Promise;
+
+exports.connectDB = () => {
+	mongoose.connect("mongodb+srv://Alkaseem:Alkaseem123@cluster0-q61lz.mongodb.net/micro-api-auth?retryWrites=true&w=majority", {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(() => console.log("Database connected!!"))
+    .catch(err => console.log(`Error: ${err}`));
+}
