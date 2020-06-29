491776a17b23a792f8d5b28b5d4b09f7d27de1b2
gosteve <nomuojamaro@gmail.com>
2020-06-29 19:57:47 +0100

    added facebook Auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,8 @@
 
 const User = require('../models/user');
 const userRouter = require('express').Router();
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const { registerValidation, loginValidation } = require('../utils/validation/joiValidation');
 // const { auth } = require('../utils/middleware');
 const {createVerificationLink} = require('../utils/EmailVerification');
@@ -96,6 +98,32 @@ userRouter.get('/logout', async (request, response) => {
 
   return response.status(200).send({
     success: true,
+  });
+});
+
+// facebook Auth
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "https://auth.microapi.dev/auth/facebook/swagger"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+app.get('/auth/facebook',
+passport.authenticate('facebook'));
+
+app.get('/auth/facebook/swagger',
+passport.authenticate('facebook', { failureRedirect: '/login' }),
+function(req, res) {
+  // Successful authentication, redirect home.
+  return response.status(200).json({
+    success: True,
+    message: 'Sucessfully logged in',
+    data: user.toJSON()
   });
 });
 
