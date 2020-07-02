f53facd10774cd5b0a41e347322ed6780ddbccae
dejetem <52535603+dejetem@users.noreply.github.com>
2020-07-02 19:30:16 +0100

    user cookie updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const userRouter = require('express').Router();
+const session = require('../app')
 
 const { 
   registerValidation, 
@@ -39,13 +40,16 @@ userRouter.post('/register', registerValidation(), async (request, response) => 
 
   user = new User({ ...request.body });
   user = await user.save();
-  res.cookie('id:', userId);
+  //res.cookie('id:', userId);
+  if (request.cookies.user_sid && request.session.user) {
+    respond.cookie('user_sid');        
+  }
+
 
   // Send a confirmation link to email
   const mailStatus = await createVerificationLink(user, request);
   console.log(mailStatus);
   const { verificationUrl } = mailStatus;
-
   return response.status(201).json({
     success: true,
     verificationUrl,
@@ -57,6 +61,7 @@ userRouter.post('/register', registerValidation(), async (request, response) => 
 userRouter.post('/login', loginValidation(), async (request, response) => {
   // Login as guest
   const { email, password } = request.body;
+  
 
   // check if user has verified email
   
@@ -86,21 +91,50 @@ userRouter.post('/login', loginValidation(), async (request, response) => {
 
   // Send token in response cookie for user session
   let client = await user.generateToken();
-
   response.cookie('w_authExp', client.tokenExp);
   response.cookie('w_auth', client.token).status(200).json({
     success: true,
     userId: client.id,
     token: client.token
   });
-  req.session.login(userInfo, function(err) {
+  /**req.session.login(userInfo, function(err) {
     if (err) {
        return res.status(500).send("There was an error logging in. Please try again later.");
     }
-  });
-  res.cookie('id:', userId);
+  });*/
+  if (request.cookies.user_sid && request.session.user) {
+     //res.cookie('user_sid');  
+     response.cookie('user_sid:', userId);      
+  }
 });
 
+/**userRouter.get('/logout', asyn (request, response) => {
+  const query = {
+    id: request.body.id
+  };
+
+  const update = {
+    token: '',
+    tokenExp: ''
+  };
+
+  await User.findOneAndUpdate(query, update);
+  /**req.session.logout(userInfo, function(err) {
+    if (err) {
+       return res.status(500).send("There was an error loggingout. Please try again later.");
+    }
+  });
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('w_auth');        
+  }
+  //clearCookie('id');
+  //req.session = null;
+  response.clearCookie('w_authExp');
+  response.clearCookie('w_auth');
+  return response.status(200).send({
+    success: true,
+  });
+});*/
 userRouter.get('/logout', async (request, response) => {
   const query = {
     id: request.body.id
@@ -112,13 +146,8 @@ userRouter.get('/logout', async (request, response) => {
   };
 
   await User.findOneAndUpdate(query, update);
-  req.session.logout(userInfo, function(err) {
-    if (err) {
-       return res.status(500).send("There was an error loggingout. Please try again later.");
-    }
-  });
-  clearCookie('id');
-  req.session = null;
+  response.clearCookie('w_authExp');
+  response.clearCookie('w_auth');
   return response.status(200).send({
     success: true,
   });
