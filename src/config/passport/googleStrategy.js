44135c244d588b2a4e286932e6edd524eab4c23f
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-29 20:04:26 +0200

    feat: add dynamic google strategy

--- src/config/passport/googleStrategy.js
+++ src/config/passport/googleStrategy.js
@@ -1,48 +1,51 @@
-const passport = require('passport');
-const GoogleUser = require('../../models/googleUser');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require("passport");
+const GoogleUser = require("../../models/googleUser");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
-
-passport.use(
-  new GoogleStrategy(
+const createGoogleStrategy = (clientID, clientSecret) => {
+  return new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: clientID || process.env.GOOGLE_CLIENT_ID,
+      clientSecret: clientSecret || process.env.GITHUB_CLIENT_SECRET,
       callbackURL: `${process.env.HOST}/api/auth/google/callback`,
     },
-    (accessToken, refreshToken, profile, done) => {
-      GoogleUser.findOne({ email: profile.emails[0].value }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        // Check if the user is available
-        if (!user) {
-          let newUser = new GoogleUser({
-            googleId: profile.id,
-            username: profile.displayName.trim(),
-            firstname: profile.name.givenName,
-            lastname: profile.name.familyName,
-            email: profile.emails[0].value,
-            isVerified: profile.emails[0].verified,
-          });
-  
-          newUser.save((err) => {
-            if (err) {
-              console.log(err);
-            }
-            console.log('===New=Google=Profile===');
-            return done(err, newUser);
-          });
-        } else {
-          console.log('===Existing=Google=Profile===');
-          // console.log(user);
-          return done(err, user);
-        }
-      });
+    callback
+  );
+};
+
+const callback = (accessToken, refreshToken, profile, done) => {
+  GoogleUser.findOne({ email: profile.emails[0].value }, (err, user) => {
+    if (err) {
+      return done(err);
     }
-  )
-);
+    // Check if the user is available
+    if (!user) {
+      let newUser = new GoogleUser({
+        googleId: profile.id,
+        username: profile.displayName.trim(),
+        firstname: profile.name.givenName,
+        lastname: profile.name.familyName,
+        email: profile.emails[0].value,
+        isVerified: profile.emails[0].verified,
+      });
+
+      newUser.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("===New=Google=Profile===");
+        return done(err, newUser);
+      });
+    } else {
+      console.log("===Existing=Google=Profile===");
+      // console.log(user);
+      return done(err, user);
+    }
+  });
+};
+
 // Persist the user
 passport.serializeUser(GoogleUser.serializeUser());
 passport.deserializeUser(GoogleUser.deserializeUser());
-  
+
+module.exports = createGoogleStrategy;
--- src/routes/googleLogin.js
+++ src/routes/googleLogin.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const User = require('../models/user');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
+const createGoogleStrategy = require("../config/passport/googleStrategy");
 const route = express.Router();
 
-route.get('/', passport.authenticate('google', { 
-  scope: [
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email'
-  ]
-})
+route.get(
+  "/",
+  passport.authenticate(createGoogleStrategy(), {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
 );
 
-route.get('/callback', 
-  passport.authenticate('google', {failureRedirect: '/api/auth/google'}), 
+route.get(
+  "/callback",
+  passport.authenticate(createGoogleStrategy(), {
+    failureRedirect: "/api/auth/google",
+  }),
   (req, res) => {
     console.log(req.user);
     res.status(200).json({
       success: true,
-      message: 'Google authenticated',
-      user: req.user
+      message: "Google authenticated",
+      user: req.user,
     });
-  });
+  }
+);
 
 module.exports = route;
