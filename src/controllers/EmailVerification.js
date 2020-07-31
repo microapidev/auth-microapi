3bc3feb642a3ed8b7fc0f4c96fbd8589dc96dde7
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-31 13:21:41 +0100

    feat: added cb url for microapidev

--- src/controllers/EmailVerification.js
+++ src/controllers/EmailVerification.js
@@ -1,5 +1,5 @@
 /**=====================================================================
- * ====   TITLE:: IMPLEMENTATION OF EMAIL VERIFICATION CONTROLLER   ====  
+ * ====   TITLE:: IMPLEMENTATION OF EMAIL VERIFICATION CONTROLLER   ====
  * ====   AUTHOR:: HACKINWALE <hackinwale.developer@gmail.com>      ====
  * ====   DATE::            28TH JUNE 2020                          ====
  * =====================================================================
@@ -8,17 +8,19 @@
 const EmailVerService = require("../services/EmailVerification");
 const response = require("../utils/response");
 
-class EmailVerification{
-
-  async verifyEmail(req, res){
-
+class EmailVerification {
+  async verifyEmail(req, res) {
     const data = await EmailVerService.verifyEmail(req);
 
-    return res.status(200).send(response("Email Address Verified", data));
+    // return res.status(200).send(response("Email Address Verified", data));
+    //hardcoding call back url to redirect back to microapi site
 
+    if (data) {
+      return res.redirect("https://microapi.dev/accounts/signin");
+    }
   }
 
-  async resendVerification(req, res){
+  async resendVerification(req, res) {
     const data = await EmailVerService.resendVerificationEmail(req);
     return res.status(200).send(response("New Verification Link Sent.", data));
   }
