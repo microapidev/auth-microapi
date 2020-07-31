ccab133c60569797743f97666e262ff0882f40f8
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-31 02:44:29 +0200

    feat: add option to return json format of docs

--- src/routes/documentation.js
+++ src/routes/documentation.js
@@ -7,9 +7,27 @@ const swaggerUiOptions = {
   customCss: ".swagger-ui .topbar { display: none }",
 };
 
+const jsonMiddleware = (req, res, next) => {
+  const { format } = req.query;
+
+  if (format && format.toString() === "json") {
+    res.send(JSON.stringify(openApiDocumentation));
+  } else {
+    next();
+  }
+};
+
 // use swagger-ui-express for your app documentation endpoint
 router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(openApiDocumentation, swaggerUiOptions));
-router.get("/docs", swaggerUi.setup(openApiDocumentation, swaggerUiOptions));
+router.get(
+  "/",
+  jsonMiddleware,
+  swaggerUi.setup(openApiDocumentation, swaggerUiOptions)
+);
+router.get(
+  "/docs",
+  jsonMiddleware,
+  swaggerUi.setup(openApiDocumentation, swaggerUiOptions)
+);
 
 module.exports = router;
