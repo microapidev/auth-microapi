42355567befb93c663f649e15b6b09c96a0ed189
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-08-01 02:54:17 +0200

    feat: add try catch

--- src/routes/googleLogin.js
+++ src/routes/googleLogin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const url = require("url");
 const { google } = require("googleapis");
+const CustomResponse = require("../utils/response");
 const createGoogleStrategy = require("../config/passport/googleStrategy");
 const {
   authorizeUser,
@@ -13,38 +14,42 @@ const route = express.Router();
 const activeCredentials = {};
 
 route.get("/", authorizeUser, googleAuthProvider, async (req, res) => {
-  const oauth2Client = new google.auth.OAuth2(
-    req.provider.clientID,
-    req.provider.clientSecret,
-    `${process.env.HOST}/api/google/callback`
-  );
+  try {
+    const oauth2Client = new google.auth.OAuth2(
+      req.provider.clientID,
+      req.provider.clientSecret,
+      `${process.env.HOST}/api/google/callback`
+    );
 
-  // Cache the credentials of the admin who owns them
-  activeCredentials[req.provider.clientID] = {
-    clientID: req.provider.clientID,
-    clientSecret: req.provider.clientSecret,
-    successCallbackUrl: req.successCallbackUrl,
-    failureCallbackUrl: req.failureCallbackUrl,
-  };
+    // Cache the credentials of the admin who owns them
+    activeCredentials[req.provider.clientID] = {
+      clientID: req.provider.clientID,
+      clientSecret: req.provider.clientSecret,
+      successCallbackUrl: req.successCallbackUrl,
+      failureCallbackUrl: req.failureCallbackUrl,
+    };
 
-  // generate a url that asks permissions for Blogger and Google Calendar scopes
-  const scopes = [
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/userinfo.email",
-  ];
+    // generate a url that asks permissions for Blogger and Google Calendar scopes
+    const scopes = [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ];
 
-  const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: "offline",
+    const url = oauth2Client.generateAuthUrl({
+      // 'online' (default) or 'offline' (gets refresh_token)
+      access_type: "offline",
 
-    // If you only need one scope you can pass it as a string
-    scope: scopes,
+      // If you only need one scope you can pass it as a string
+      scope: scopes,
 
-    // Add the client ID to ensure the correnct credentials are used on server response (callback)
-    state: req.provider.clientID,
-  });
+      // Add the client ID to ensure the correnct credentials are used on server response (callback)
+      state: req.provider.clientID,
+    });
 
-  return res.json({ redirectUrl: url });
+    res.json({ redirectUrl: url });
+  } catch {
+    res.status(500).json(CustomResponse("InternalServerError"));
+  }
 });
 
 route.get(
