ac3f69abe03449588013353af57e9aaddf6b8f92
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-08-04 10:10:09 +0100

    revert eslint to config to initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   rules: {
     indent: ["error", 2, { SwitchCase: 1 }],
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     "no-console": [
