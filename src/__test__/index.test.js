8502f94e4444066966a0bbacb68719d373d67399
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-08-06 01:04:13 +0200

    feat: add initial setup for swagger with example

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -2,9 +2,10 @@ import app from "../";
 import request from "supertest";
 
 describe("Server runs", () => {
-  it("should return a status of 200", async () => {
-    const response = await request(app).get("/");
-    expect(response.status).toBe(200);
+  test("Should return status 200 and render Swagger docs", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.text).toMatchSnapshot();
   });
   it("should return a status of 404", async () => {
     const response = await request(app).get("/garbledygook");
---
+++ src/utils/swaggerSpec.js
@@ -0,0 +1,82 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+// List of tags used by documentation.
+export const GENERAL_TAG = "General";
+export const ADMINISTRATOR_TAG = "Administrators";
+export const USER_TAG = "Users";
+export const AUTH_PROVIDER_TAG = "Authentication Providers";
+
+// Security schema.
+export const apiKeySecurity = [{ Bearer: {} }];
+
+// Swagger definition
+const swaggerDefinition = {
+  info: {
+    openapi: "3.0.21", // Version of swagger
+    title: "Authentication MicroAPI", // Title of the documentation
+    version: "2.0.0", // Version of the app
+    description:
+      "Authenticate all your users with local (email and password) and social authentication (Google, Twitter, Facebook, and GitHub). Get started and give your users a smooth authentication flow.", // short description of the app
+    termsOfService: "https://microapi.dev/terms_of_service",
+    contact: {
+      name: "API Support",
+      url: "https://microapi.dev/contact_us",
+      email: "api@auth.microapi.dev",
+    },
+    license: {
+      name: "MIT",
+      url: "https://github.com/microapidev/auth-microapi/blob/v2/LICENSE",
+    },
+  },
+  servers: [
+    {
+      url: "https://auth.microapi.dev",
+      description: "Production server (uses live data)",
+    },
+    {
+      url: "https://authentication-microapi-v2.herokuapp.com",
+      description: "Staging server (uses test data)",
+    },
+    {
+      url: "http://localhost:3000",
+      description: "Local server (uses test data)",
+    },
+  ],
+  tags: [
+    {
+      name: "admins",
+      description: "Create and manage administrators.",
+    },
+    {
+      name: "users",
+      description: "Create and manage users.",
+    },
+    {
+      name: "providers",
+      description:
+        "Authenticate using one of the supported authentication providers.",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      Bearer: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ["src/jsdocs/**/*.yaml"],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
