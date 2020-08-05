c92edfec54ea43f1f88920801c6362fc54a7f01c
Dave <callremz@gmail.com>
2020-08-05 14:26:38 +0100

    chore: configure babel

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
+  ignore: ["**/*.test.js"],
+};
