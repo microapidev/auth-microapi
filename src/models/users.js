b77099442a265a0697a18e5c599c5c40b11fe5f1
piouson <piouson@gmail.com>
2020-08-06 11:16:19 +0100

    feat: add a user model
    - add user schema
    
    Resolve #531

---
+++ src/models/users.js
@@ -0,0 +1,78 @@
+/**
+ * A representation of an authenticating user
+ * @module models
+ * @author piouson <https://github.com/piouson>
+ */
+const mongooose = require("mongoose");
+
+/**
+ * User schema
+ * @constructor User
+ */
+const userSchema = new mongooose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  phone: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    isEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+    }
+  },
+  authProvider: [{
+    authId: {
+      type: String,
+    },
+    authType: {
+      type: String,
+      enum: ["facebook", "github", "google", "twitter"],
+    },
+  }],
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  failedLoginAttempts: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+    lastAttempt: {
+      type: Date,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },  
+}, { timestamps: true });
+
+module.exports = mongooose.model("User", userSchema);
