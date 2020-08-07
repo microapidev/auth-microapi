80e8087dad268587bce87a805d3115ca584c8cc1
piouson <piouson@gmail.com>
2020-08-07 12:04:25 +0100

    Update models index

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,5 @@
-<<<<<<< Updated upstream
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const IndexSchema = new Schema({
-  name: {
-    type: String,
-    default: "A name",
-  },
-});
-
-module.exports = mongoose.model("Index", IndexSchema);
-=======
 const UserModel = require("./users");
 
 module.exports = {
   UserModel,
 };
->>>>>>> Stashed changes
